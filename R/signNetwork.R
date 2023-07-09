8e74ecffb26555e10719d7cabc3b7e206cbb8026
mschalberger <marc_schalberger@t-online.de>
2023-07-09 22:32:36 +0200

    Fix signNetwork

--- R/signNetwork.R
+++ R/signNetwork.R
@@ -15,16 +15,14 @@
 
 
 signNetwork <- function(mat, directed = F, loops = F, matrix.type, cov = NULL, names = NULL, ...) {
-  if (is.list(mat)) {
-    i <- 0
+  if (is.list(mat)&& !is.data.frame(mat)) {
     nets <- lapply(mat, function (x) {
-      i <- i + 1
       if (matrix.type == "adjacency") {
         if (directed == F){
           if (isSymmetric(x)) {
             x
           } else if (all.equal(x[lower.tri(x)], rep(0,(nrow(x)^2-nrow(x))/2)) == T || #lower triangle is all zeros or
-              all.equal(x[lower.tri(x)], rep(NA,(nrow(x)^2-nrow(x))/2)) == T) { #lower triangle is all NAs
+                     all.equal(x[lower.tri(x)], rep(NA,(nrow(x)^2-nrow(x))/2)) == T) { #lower triangle is all NAs
             x[is.na(x)] <- 0
             x <- x + t(x)
           } else {
@@ -55,11 +53,14 @@ signNetwork <- function(mat, directed = F, loops = F, matrix.type, cov = NULL, n
         full <- rbind(edges_pos, edges_neg)
         dyn_net <- as.network(full, matrix.type = "edgelist", directed = directed, loops = loops, ... = ...)
       }
-      if(is.null(names)) {
-        if (matrix.type == "adjacency") {
-          tryCatch({network.vertex.names(dyn_net) <- rownames(x)}, error = function(e){})
-        }
-      } else {
+      # if(is.null(names)) {
+      #   if (matrix.type == "adjacency") {
+      #     tryCatch({network.vertex.names(dyn_net) <- rownames(x)}, error = function(e){})
+      #   }
+      # } else {
+      #   network.vertex.names(dyn_net) <- names[[i]]
+      # }
+      if(!is.null(names)) {
         network.vertex.names(dyn_net) <- names[[i]]
       }
 
@@ -83,69 +84,73 @@ signNetwork <- function(mat, directed = F, loops = F, matrix.type, cov = NULL, n
     class(nets) <- "dynamic.sign"
     return(nets)
   }
-  else if (is.matrix(mat)) {
-  if (matrix.type == "adjacency") {
-    if (directed == F){
-      if (isSymmetric(mat)) {
-        mat
-      } else if (all.equal(mat[lower.tri(mat)], rep(0,(nrow(mat)^2-nrow(mat))/2)) == T || #lower triangle is all zeros or
-          all.equal(mat[lower.tri(mat)], rep(NA,(nrow(mat)^2-nrow(mat))/2)) == T) { #lower triangle is all NAs
-        mat[is.na(mat)] <- 0
-        mat <- mat + t(mat)
-      } else {
-        stop("Matrix is not symmetric!")
+  else  {
+    if (matrix.type == "adjacency") {
+      if (directed == F){
+        if (isSymmetric(mat)) {
+          mat
+        } else if (all.equal(mat[lower.tri(mat)], rep(0,(nrow(mat)^2-nrow(mat))/2)) == T || #lower triangle is all zeros or
+                   all.equal(mat[lower.tri(mat)], rep(NA,(nrow(mat)^2-nrow(mat))/2)) == T) { #lower triangle is all NAs
+          mat[is.na(mat)] <- 0
+          mat <- mat + t(mat)
+        } else {
+          stop("Matrix is not symmetric!")
+        }
+      }
+      if (loops == F){
+        diag(mat) <-0
+      }
+
+      static_net <- as.network(abs(mat), matrix.type = "adjacency", directed = directed, loops = loops, ... = ...)
+      static_net <- set.edge.value(static_net, "sign", mat)
+      pos <- (static_net%e%"sign" == 1)
+      neg <- (static_net%e%"sign" == -1)
+      static_net%e%"pos" <- pos
+      static_net%e%"neg" <- neg
+
+    } else if (matrix.type == "edgelist"){
+      mat <- as.data.frame(mat)
+      colnames(mat) <- c("from","to","sign")
+      edges_pos <- mat[mat[,3]== 1,]
+      tryCatch({
+        edges_pos$pos = TRUE
+        edges_pos$neg = FALSE}, error = function(e){})
+      edges_neg <- mat[mat[,3]== -1,]
+      tryCatch({
+        edges_neg$pos = FALSE
+        edges_neg$neg = TRUE}, error = function(e){})
+      static_net <- as.network(rbind(edges_pos, edges_neg), matrix.type = "edgelist", directed = directed, loops = loops, ... = ...)
+    }
+
+    # if (is.null(names)) {
+    #   if (matrix.type == "adjacency") {
+    #     tryCatch({network.vertex.names(static_net) <- rownames(mat)}, error = function(e){})
+    #   }
+    # } else {
+    #   network.vertex.names(static_net) <- names
+    # }
+
+    if (!is.null(names)) {
+      network.vertex.names(static_net) <- names
+    }
+
+    if (!is.null(cov)) {
+      # match the names in the network.vertex.names(net) list with the names in the first column of the cov data frame
+      match_indices <- match(network.vertex.names(static_net), cov[,1])
+
+      # subset the cov data frame based on the match indices
+      subset_cov <- cov[match_indices, ]
+
+      # sort the subset_cov data frame based on the network.vertex.names(net) list
+      subset_cov <- subset_cov[order(match(subset_cov[,1], network.vertex.names(static_net))), ]
+
+      for(col in colnames(subset_cov)[-1]){
+        set.vertex.attribute(static_net, col, subset_cov[,col])
       }
     }
-    if (loops == F){
-      diag(mat) <-0
-    }
 
-    static_net <- as.network(abs(mat), matrix.type = "adjacency", directed = directed, loops = loops, ... = ...)
-    static_net <- set.edge.value(static_net, "sign", mat)
-    pos <- (static_net%e%"sign" == 1)
-    neg <- (static_net%e%"sign" == -1)
-    static_net%e%"pos" <- pos
-    static_net%e%"neg" <- neg
-
-  } else if (matrix.type == "edgelist"){
-    mat <- as.data.frame(mat)
-    colnames(mat) <- c("from","to","sign")
-    edges_pos <- mat[mat[,3]== 1,]
-    tryCatch({
-      edges_pos$pos = TRUE
-      edges_pos$neg = FALSE}, error = function(e){})
-    edges_neg <- mat[mat[,3]== -1,]
-    tryCatch({
-      edges_neg$pos = FALSE
-      edges_neg$neg = TRUE}, error = function(e){})
-    static_net <- as.network(rbind(edges_pos, edges_neg), matrix.type = "edgelist", directed = directed, loops = loops, ... = ...)
-  }
-
-  if (is.null(names)) {
-    if (matrix.type == "adjacency") {
-      tryCatch({network.vertex.names(static_net) <- rownames(mat)}, error = function(e){})
-    }
-  } else {
-    network.vertex.names(static_net) <- names
-  }
-
-  if (!is.null(cov)) {
-    # match the names in the network.vertex.names(net) list with the names in the first column of the cov data frame
-    match_indices <- match(network.vertex.names(static_net), cov[,1])
-
-    # subset the cov data frame based on the match indices
-    subset_cov <- cov[match_indices, ]
-
-    # sort the subset_cov data frame based on the network.vertex.names(net) list
-    subset_cov <- subset_cov[order(match(subset_cov[,1], network.vertex.names(static_net))), ]
-
-    for(col in colnames(subset_cov)[-1]){
-      set.vertex.attribute(static_net, col, subset_cov[,col])
-    }
-  }
-
-  class(static_net) <- "static.sign"
-  return(static_net)
+    class(static_net) <- "static.sign"
+    return(static_net)
   }
 }
 
