f574a8f8d18e1dbc85034aa3ec471b9c0fc27eb0
mschalberger <marc_schalberger@t-online.de>
2024-03-22 21:38:52 +0100

    Update roxygen

--- R/ergm.sign-package.R
+++ R/ergm.sign-package.R
@@ -1,3 +1,6 @@
+#' @keywords internal
+"_PACKAGE"
+
 #' ergm.sign: A Package for Exponential Random Graph Models for Signed Networks
 #'
 #' The ergm.sign package implements the tools to simulate and estimate Signed Exponential Random Graph Models and Temporal Signed Exponential Random Graph Models.
@@ -13,7 +16,5 @@
 #' @importFrom  tergm NetSeries tergm control.tergm
 #' @importFrom Rdpack reprompt
 #' @importFrom dplyr bind_rows
-#'
-#' @docType package
 #' @name ergm.sign
 NULL
