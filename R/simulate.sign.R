7759c988fea2da5d782c72b8d52ebaecfc41de95
Marc Schalberger <marc_schalberger@t-online.de>
2026-04-30 14:12:00 +0200

    Correct class assignment for NetList

--- R/simulate.sign.R
+++ R/simulate.sign.R
@@ -1,21 +1,25 @@
 #' @export
 simulate.sign <- function(object, nsim = 1, seed = NULL, ...) {
   net <- object$network
+  is_dynamic <- "dynamic.sign" %in% class(net)
 
   raw_sims <- NextMethod()
-
+if(is_dynamic) {
   if(nsim == 1) {
     sim <- raw_sims
     nwl <- uncombine_network(sim)
-    #nwl <- c(nwl[[1L]] %n% ".PrevNets", nwl)
+    nwl <- lapply(nwl, function(x) {class(x) <- c("static.sign",class(x)); return(x)})
     sim %n% "NetList" <- nwl
   } else {
     sim <- lapply(raw_sims, function(x) {
       nwl <- uncombine_network(x)
-      #nwl <- c(nwl[[1L]] %n% ".PrevNets", nwl)
+      nwl <- lapply(nwl, function(y) {class(y) <- c("static.sign",class(y)); return(y)})
       x %n% "NetList" <- nwl
       return(x)
     })
   }
+  }else {
+  sim <- raw_sims
+}
 
 }
